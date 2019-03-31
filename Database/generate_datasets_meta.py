bc0b3a7203b8ec494946e0fd04c2578236605afc
Zhijie Cao <caozj@mail.cbi.pku.edu.cn>
2019-03-31 15:54:35 +0800

    Minor changes

--- Database/generate_datasets_meta.py
+++ Database/generate_datasets_meta.py
@@ -17,14 +17,51 @@ used_columns = (
     "dataset_name", "organism", "organ", "lifestage", "platform",
     "cell number", "publication", "remark"
 )
-single = pd.read_csv("../../Datasets/ACA_datasets.csv", comment="#", skip_blank_lines=True).loc[:, used_columns]
-additional = pd.read_csv("../../Datasets/additional_datasets.csv", comment="#", skip_blank_lines=True).loc[:, used_columns]
-aligned = pd.read_csv("../../Datasets/aligned_datasets.csv", comment="#", skip_blank_lines=True).loc[:, used_columns]
+single = pd.read_csv(
+    "../../Datasets/ACA_datasets.csv",
+    comment="#", skip_blank_lines=True
+).loc[:, used_columns]
+additional = pd.read_csv(
+    "../../Datasets/additional_datasets.csv",
+    comment="#", skip_blank_lines=True
+).loc[:, used_columns]
+single = pd.concat([single, additional], axis=0, ignore_index=True)
+aligned = pd.read_csv(
+    "../../Datasets/aligned_datasets.csv",
+    comment="#", skip_blank_lines=True
+).loc[:, used_columns]
 
-combined = pd.concat([single, additional, aligned], axis=0)
-combined = combined.loc[np.in1d(combined["dataset_name"], list(dataset_dict.keys())), :]
-combined["visualization"] = np.vectorize(lambda x: ", ".join(dataset_dict[x]))(combined["dataset_name"])
+for idx, row in aligned.iterrows():
+    aligned.loc[idx, "cell number"] = single.loc[np.in1d(
+        single["dataset_name"], row["remark"].split(", ")
+    ), "cell number"].sum()
+
+combined = pd.concat([single, aligned], axis=0, ignore_index=True)
+combined = combined.loc[np.in1d(
+    combined["dataset_name"], list(dataset_dict.keys())
+), :]
+combined["cell number"] = combined["cell number"].astype(np.int)
+
+combined["self-projection coverage"] = np.nan
+combined["self-projection accuracy"] = np.nan
+for idx, row in combined.iterrows():
+    spf_path = os.path.join(row["dataset_name"], "self_projection.txt")
+    if not os.path.exists(spf_path):
+        print("Missing: " + spf_path)
+    else:
+        with open(spf_path, "r") as spf:
+            lines = spf.readlines()
+            k1, v1 = lines[0].split()
+            k2, v2 = lines[1].split()
+            assert k1 == "coverage" and k2 == "accuracy"
+            v1, v2 = float(v1.strip()), float(v2.strip())
+            combined.loc[idx, "self-projection coverage"] = v1
+            combined.loc[idx, "self-projection accuracy"] = v2
+
+combined["visualization"] = np.vectorize(
+    lambda x: ", ".join(dataset_dict[x])
+)(combined["dataset_name"])
 
 combined.to_csv("./datasets_meta.csv", index=False)
-combined.to_json("./datasets_meta.json", orient="records")
+combined.to_json("./datasets_meta.json", orient="records", double_precision=3)
 
